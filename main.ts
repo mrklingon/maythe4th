426590a8a09b3b498e8257849e05dbab0a61174d
Joel Peter Anderson <mrklingon@users.noreply.github.com>
2023-05-04 12:45:13 -0500

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,39 @@
+let ships = [
+"Kalee Bastion",
+"Gand Navigator",
+"Calamity",
+"Harmony",
+"Blade",
+"Beast",
+"Mandalorian Mercenary",
+"Naboo Brute",
+"Devaron Marauder",
+"Messenger"
+]
+let planets = [
+"Lelsodi",
+"Asem",
+"Chevu",
+"Russuc",
+"Okind",
+"Truth",
+"Tor",
+"Urre",
+"Vit",
+"Yus"
+]
+let jedi = [
+"Dansinc Romerai",
+"Calrya Popwadh",
+"Micdom Connbene",
+"Josbray Aswgrah",
+"Rylejov Cradeer",
+"Coopalfr Belcros",
+"Coleconn Haynnor",
+"Jortrav Plarode",
+"Giantyle Permilb",
+"Brygabr Vinerr"
+]
+basic.forever(function () {
+	
+})
